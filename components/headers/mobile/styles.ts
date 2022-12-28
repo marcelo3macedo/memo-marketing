ee9f3ec5a75dc598f4c6a158948485cb752af21f
Marcelo Macedo <marcelo3macedo@gmail.com>
2022-12-28 08:49:02 -0300

    feat: SEO added

--- components/headers/mobile/styles.ts
+++ components/headers/mobile/styles.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     border-bottom: 2px solid var(--neutral60);
+
+    @media (min-width: 768px) {
+        display: none;
+    }
 `;
 
 export const Container = styled.main`
--- components/headers/top/index.tsx
+++ components/headers/top/index.tsx
@@ -9,6 +9,14 @@ const HeaderTop = () => {
             <title>{t('application.title')}</title>
             <link rel="icon" href="logo.png" />
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            <meta name='description' content={t('application.description')} />
+            <meta property='og:title' content={t('application.title')} />
+            <meta
+                property='og:description'
+                content={t('application.description')}
+            />
+            <meta property='og:url' content='https://www.memorizou.com.br/' />
+            <meta property='og:type' content='website' />
         </Head>
     )
 }
